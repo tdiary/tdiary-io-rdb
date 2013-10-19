17b2ea69c6feb28e2460079c56aedac57e1ad3a0
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-10-19 17:08:36 +0900

    bundle gem tdiary-io-rdb -t

---
+++ lib/tdiary/io/rdb.rb
@@ -0,0 +1,9 @@
+require "tdiary/io/rdb/version"
+
+module Tdiary
+  module Io
+    module Rdb
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/tdiary/io/rdb/version.rb
@@ -0,0 +1,7 @@
+module Tdiary
+  module Io
+    module Rdb
+      VERSION = "0.0.1"
+    end
+  end
+end
