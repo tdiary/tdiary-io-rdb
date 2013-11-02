0bf40fbe4f6c19378a373d3a057dcac8301fa95f
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-11-02 22:10:33 +0900

    added load/save cgi conf specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,15 @@ require 'tdiary/comment_manager'
 require 'tdiary/referer_manager'
 require 'tdiary/cache/file'
 require 'tdiary/io/rdb'
+
+class Dummy
+  def database_url
+    'sqlite://./tdiary_test.db'
+  end
+end
+
+RSpec.configure do |c|
+  c.after(:suite) do
+    FileUtils.rm_rf File.expand_path('../../tdiary_test.db', __FILE__)
+  end
+end
--- spec/tdiary/io/rdb_spec.rb
+++ spec/tdiary/io/rdb_spec.rb
@@ -4,4 +4,25 @@ describe TDiary::IO::Rdb do
   it 'is_a TDiary::IO::Base' do
     expect { TDiary::IO::Rdb.is_a?(TDiary::IO::Base) }.to be_true
   end
+
+  describe "#save_cgi_conf and #load_cgi_conf" do
+    let(:conf) { Dummy.new }
+
+    it { expect(TDiary::IO::Rdb.load_cgi_conf(conf)).to be_empty }
+
+    context "given body" do
+      before do
+        TDiary::IO::Rdb.save_cgi_conf(conf, 'foo')
+      end
+
+      it { expect(TDiary::IO::Rdb.load_cgi_conf(conf)).to eq 'foo' }
+
+      context "update" do
+        before do
+          TDiary::IO::Rdb.save_cgi_conf(conf, 'bar')
+        end
+        it { expect(TDiary::IO::Rdb.load_cgi_conf(conf)).to eq 'bar' }
+      end
+    end
+  end
 end
